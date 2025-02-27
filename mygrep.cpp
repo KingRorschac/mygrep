e821d70da9b6f3e942cf84949ced37b5138428b8
Joonas Akers <joonas.akers@tuni.fi>
2025-02-27 17:25:45 +0200

    mygrepProjekti_JoonasAkers_3inkrementti_4piste

--- mygrep.cpp
+++ mygrep.cpp
@@ -71,6 +71,52 @@ int main(int argc, char* argv[]) {
             cout << "Pattern not found" << endl;
         }
     }
+    if (argc == 4) {
+        teksti = argv[3];
+        etsittäväSana = argv[2];
+        string type = argv[1];
+
+        ifstream inputFile(teksti);
+
+        // Check if the file is open
+        if (inputFile.is_open()) {
+            string line;
+            // Read each line of the file into the vector
+            while (getline(inputFile, line)) {
+                teksti2.push_back(line);
+            }
+            inputFile.close();
+        }
+        else {
+            cerr << "Unable to open file" << endl;
+            return 1;
+        }
+
+        // Find the etsittäväSana in the teksti
+        size_t paikka = teksti.find(etsittäväSana);
+        if (type == "-olo" || type == "-ol" || type == "-oo") {
+            bool found = false;
+            int j = 0;
+            for (size_t i = 0; i < teksti2.size(); ++i) {
+                size_t position = teksti2[i].find(etsittäväSana);
+                if (position != string::npos) {
+                    if (type == "-olo" || type == "-oo") {
+                        cout << i + 1 << ": ";
+                    }
+                    cout << teksti2[i] << endl;
+                    j++;
+                    found = true;
+                }
+            }
+            if (type == "-olo" || type == "-ol") {
+                cout << endl << "Occurrences of lines containing ''" << etsittäväSana << "'': " << j << endl;
+            }
+
+            if (!found) {
+                cout << "Pattern not found" << endl;
+            }
+        }
+    }
 
     return 0;
 }
